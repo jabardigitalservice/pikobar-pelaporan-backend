74c1ca57e6d8a8c147db9d7582c98006308e4159
firmanjs <fimzhakim@gmail.com>
2020-10-06 18:28:03 +0700

    test same path

--- api/history_travel/handlers.js
+++ api/history_travel/handlers.js
@@ -1,65 +1,59 @@
-const replyHelper = require('../helpers');
+const { replyJson } = require('../helpers')
+/**
+ * /api/history-travel
+ * @param {*} request
+ * @param {*} reply
+*/
+const createHistoryTravel = (server) => {
+  return (request, reply) => {
+    server.methods.services.history_travel.create(
+      request.payload,
+      request.params.id_case,
+      (err, result) => {
+        replyJson(err, result, reply)
+      }
+    )
+  }
+}
 
-module.exports = (server) => {
-  function historyTravelResponse(historyTravel) {
-    let jsonHistoryTravel = {
-      status: 200,
-      message: "Success",
-      data: historyTravel
-    }
-    return jsonHistoryTravel
-  };
-  return {
-    /**
-     * POST /api/history-travel
-     * @param {*} request
-     * @param {*} reply
-     */
-    async createHistoryTravel(request, reply) {
-      server.methods.services.history_travel.create(
-        request.payload,
-        request.params.id_case,
-        (err, result) => {
-          if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-          return reply(
-            historyTravelResponse(result, request)
-          ).code(200)
-        }
-      )
-    },
-    async getHistoryTravel(request, reply) {
-      server.methods.services.history_travel.read(
-        request.params.id_case,
-        (err, result) => {
-        if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-          return reply(
-            historyTravelResponse(result, request)
-          ).code(200)
-        }
-      )
-    },
-    async updateHistoryTravel(request, reply) {
-      server.methods.services.history_travel.update(
-        request.params.id_history_travel,
-        request.payload,
-        (err, result) => {
-          if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-          return reply(
-            historyTravelResponse(result, request)
-          ).code(200)
-        }
-      )
-    },
-    async deleteHistoryTravel(request, reply) {
-      server.methods.services.history_travel.delete(
-        request.params.id_history_travel,
-        (err, result) => {
-          if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-          return reply(
-            historyTravelResponse(result, request)
-          ).code(200)
-        }
-      )
-    }
-  };
+const getHistoryTravel = (server) => {
+  return (request, reply) => {
+    server.methods.services.history_travel.read(
+      request.params.id_case,
+      (err, result) => {
+        replyJson(err, result, reply)
+      }
+    )
+  }
+}
+
+const updateHistoryTravel = (server) => {
+  return (request, reply) => {
+    server.methods.services.history_travel.update(
+      request.params.id_history_travel,
+      request.payload,
+      (err, result) => {
+        replyJson(err, result, reply)
+      }
+    )
+  }
+}
+
+const deleteHistoryTravel = (server) => {
+  return (request, reply) => {
+    const { id_history_travel } = request.params
+    server.methods.services.history_travel.delete(
+      id_history_travel,
+      (err, result) => {
+        replyJson(err, result, reply)
+      }
+    )
+  }
+}
+
+module.exports = {
+  createHistoryTravel,
+  updateHistoryTravel,
+  getHistoryTravel,
+  deleteHistoryTravel
 }
--- api/history_travel/routes.js
+++ api/history_travel/routes.js
@@ -1,54 +1,28 @@
 module.exports = (server) => {
-  const handlers = require('./handlers')(server)
+  const handlers = require('./handlers')
   const CheckRoleView = require('../users/route_prerequesites').CheckRoleView(server);
   const CheckRoleCreate = require('../users/route_prerequesites').CheckRoleCreate(server)
   const CheckRoleUpdate = require('../users/route_prerequesites').CheckRoleUpdate(server)
   const CheckRoleDelete = require('../users/route_prerequesites').CheckRoleDelete(server)
 
-  return [
-    {
-      method: 'POST',
-      path: '/history-travel/{id_case}',
+  const apiPath = (method, path, callback, role) => {
+    return {
+      method: method,
+      path: path,
       config: {
-        auth: 'jwt',
-        description: 'create history-travel',
+        description: `${method} history-travel`,
         tags: ['api', 'history-travel'],
-        pre: [ CheckRoleCreate ]
-      },
-      handler: handlers.createHistoryTravel
-    },
-    {
-      method: 'GET',
-      path: '/history-travel/{id_case}',
-      config: {
+        pre: [ role ],
         auth: 'jwt',
-        description: 'show list history-travel',
-        tags: ['api', 'history-travel'],
-        pre: [ CheckRoleView ]
       },
-      handler:  handlers.getHistoryTravel
-    },
-    {
-      method: 'PUT',
-      path: '/history-travel/{id_history_travel}',
-      config: {
-        auth: 'jwt',
-        description: 'update history-travel',
-        tags: ['api', 'history-travel'],
-        pre: [ CheckRoleUpdate ],
-      },
-      handler:  handlers.updateHistoryTravel
-    },
-    {
-      method: 'DELETE',
-      path: '/history-travel/{id_history_travel}',
-      config: {
-        auth: 'jwt',
-        description: 'delete history-travel',
-        tags: ['api', 'history-travel'],
-        pre: [ CheckRoleDelete ],
-      },
-      handler: handlers.deleteHistoryTravel
+      handler: handlers[callback](server),
     }
+  }
+
+  return [
+    apiPath('GET', '/history-travel/{id_case}', 'getHistoryTravel', CheckRoleView),
+    apiPath('POST', '/history-travel/{id_case}', 'createHistoryTravel', CheckRoleCreate),
+    apiPath('PUT', '/history-travel/{id_history_travel}', 'updateHistoryTravel', CheckRoleUpdate),
+    apiPath('DELETE', '/history-travel/{id_history_travel}', 'deleteHistoryTravel', CheckRoleDelete),
   ]
 }
