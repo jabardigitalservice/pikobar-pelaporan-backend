a78c58eeb189e1a6d0e7e67479023eeef107fad0
firmanjs <fimzhakim@gmail.com>
2020-10-07 15:45:10 +0700

    breakdown function aggregate

--- helpers/aggregate/func/index.js
+++ helpers/aggregate/func/index.js
@@ -41,6 +41,55 @@ const grupFunc = (grouping) => {
   return { $group: { _id: grouping, "total": { $sum: 1 }}}
 }
 
+const sumActive = (criteria) => {
+  return {
+    $sum: {
+      $cond: [
+        {
+          $and: [
+            { $eq: ["$final_result", "4"] },
+            { $eq: ["$status", criteria] },
+            {
+              $or: [
+                { $in: ["$last_history.current_location_type", ["RUMAH","RS","OTHERS"]] }
+              ]
+            }
+          ],
+        }, 1, 0]
+    }
+  }
+}
+
+const sumSick = (criteria, type) => {
+  return {
+    $sum: {
+      $cond: [
+        {
+          $and: [
+            { $eq: ["$final_result", "4"] },
+            { $eq: ["$status", criteria] },
+            { $eq: ["$last_history.current_location_type", type] },
+          ]
+        }, 1, 0]
+    }
+  }
+}
+
+const sumCondition = (criteria, status) => {
+  return {
+    $sum: {
+      $cond: [
+        {
+          $and: [
+            { $eq: ["$status", criteria] },
+            { $eq: ["$final_result", status] }
+          ]
+        }, 1, 0]
+    }
+  }
+}
+
 module.exports = {
-  sumFunc, grupFunc, sumBetweenFunc, sumWeeklyFunc
+  sumFunc, grupFunc, sumBetweenFunc, sumWeeklyFunc,
+  sumActive, sumSick, sumCondition
 }
