f099a5c0de3cb8c93e2aad69810d90d4e9f31873
firmanjs <fimzhakim@gmail.com>
2020-10-14 10:54:59 +0700

    route new version

--- api/history_travel/routes.js
+++ api/history_travel/routes.js
@@ -1,20 +1,20 @@
 module.exports = (server, handlers, roles, route) => {
   return [
     route(
-      'GET', '/history-travel/{id_case}', 'history-travel',
-      handlers.getHistoryTravel(server), roles(server).view
-    ),
-    route(
       'POST', '/history-travel/{id_case}', 'history-travel',
       handlers.createHistoryTravel(server), roles(server).create
     ),
     route(
-      'PUT', '/history-travel/{id_history_travel}', 'history-travel',
-      handlers.updateHistoryTravel(server), roles(server).update
+      'GET', '/history-travel/{id_case}', 'history-travel',
+      handlers.getHistoryTravel(server), roles(server).view
     ),
     route(
       'DELETE', '/history-travel/{id_history_travel}', 'history-travel',
       handlers.deleteHistoryTravel(server), roles(server).delete
     ),
+    route(
+      'PUT', '/history-travel/{id_history_travel}', 'history-travel',
+      handlers.updateHistoryTravel(server), roles(server).update
+    )
   ]
 }
