523bee45af9b3d8f538b58256d550e9225862c19
firmanjs <fimzhakim@gmail.com>
2020-10-27 10:15:17 +0700

    change ocupation handler

--- api/occupations/handlers.js
+++ api/occupations/handlers.js
@@ -1,48 +1,25 @@
-const replyHelper = require('../helpers')
+const { replyJson } = require('../helpers')
+const { queryIfSame, funcCreate } = require('../../helpers/request')
 
-module.exports = (server) => {
-  function constructAreasResponse(occupations) {
-    let jsonOccupations = {
-      status: 200,
-      message: "Success",
-      data: occupations
-    }
-    return jsonOccupations
+/**
+ * GET /api/occupations
+ */
+const ListOccupation = (server) => {
+  return async (request, reply) => {
+    await queryIfSame(
+      server, "occupations", "getOccupation",
+      request, reply, replyJson
+    )
   }
-
-  return {
-    /**
-     * GET /api/occupations
-     * @param {*} request
-     * @param {*} reply
-     */
-    async ListOccupation(request, reply) {
-      server.methods.services.occupations.getOccupation(
-        request.query,
-        (err, result) => {
-          if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-          return reply(
-            constructAreasResponse(result)
-          ).code(200)
-        }
-      )
-    },
-
-    /**
-     * GET /api/occupations/{id}
-     * @param {*} request
-     * @param {*} reply
-     */
-    async GetOccupationDetail(id, reply) {
-      server.methods.services.occupations.getOccupationDetail(
-        id,
-        (err, occupations) => {
-          if (err) return reply(replyHelper.constructErrorResponse(err)).code(422)
-          return reply(
-            constructAreasResponse(occupations)
-          ).code(200)
-        }
-      )
-    },
-  }//end
 }
+
+const GetOccupationDetail = (server) => {
+  return async (request, reply) => {
+    await funcCreate(
+      server, "occupations", "getOccupationDetail",
+      request, reply, replyJson
+    )
+  }
+}
+
+module.exports = { ListOccupation, GetOccupationDetail }
